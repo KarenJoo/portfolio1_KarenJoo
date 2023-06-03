d5e4d5afc636d694c8c112aefa0733aa935c24dd
Karen Jo <karenfiksdahl@gmail.com>
2023-06-03 15:22:28 +0200

    adjusting the js for the side menu

--- js/script.js
+++ js/script.js
@@ -1,46 +1,47 @@
-// scroll function side-menu and reopen menu box (chatGPT)
+// Function to ensure that the side menu is hidden when scrolling on or above the wrapper and displayed when scrolling below the wrapper.
+//Function 'toggleSideMenu to toggle the visibility of the side menu based on scroll position (chatGPT)
+function toggleSideMenu() {
+  const sideMenu = document.getElementById("side-menu");
+  const reopenBox = document.getElementById("reopen-menu");
+  const wrapperDisplay = document.getElementById("wrapper").offsetTop;
+  //scrollTop and pageYOffset to to get the current scroll position (chatGPT)
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-// add eventlistener for scroll on body
-//using pageYOffset if user scrolles below the visible area of the page
-// toggle > if scrolled is true the css class 'scrolled' is added if false it removes the css class
-window.addEventListener('scroll', function() {
-    var body = document.querySelector('body');
-    var scrolled = window.pageYOffset > window.innerHeight;
-    body.classList.toggle('scrolled', scrolled);
-  });
+  // determining when to display/not display the side menu using wrapperDisplay (.offsetTop)
+  // scrolling past wrapper the reopen side menu is visible
+  if (scrollTop > wrapperDisplay) {
+    sideMenu.style.left = "-300px"; // Hide the side menu by moving it off-screen
+    reopenBox.style.display = "block"; // Show the reopen box
+  } else {
+    sideMenu.style.left = "-300px"; // Hide the side menu by moving it off-screen
+    reopenBox.style.display = "none"; // Hide the reopen box
+  }
+}
 
-// toggle on the reopen menu box to display similar to the side-menu
+// Event listener for scroll event
+window.addEventListener("scroll", toggleSideMenu);
 
 // side menu close button
-//  using event listener to the close button, when clicked, the side menu moves off-screen by left property -300px.
-const sideMenu = document.getElementById("side-menu");
+// using event listener for the close button, when clicked, the side menu moves off-screen by setting the left property to -300px.
 const closeButton = document.getElementById("close-button");
 const reopenSideMenu = document.getElementById("reopen-menu");
 
 closeButton.addEventListener("click", () => {
-  sideMenu.style.left = "-300px"; //moving the sidemenu off screen
+  closeSideMenu();
+  reopenSideMenu.style.display = "block"; // Show the reopen menu when the side menu is closed
 });
 
 reopenSideMenu.addEventListener("click", () => {
-  sideMenu.style.left = "0"; // Move the menu back to its original position
+  openSideMenu();
+  reopenSideMenu.style.display = "none"; // Hide the reopen menu when the side menu is open
 });
 
 // Function to open the side menu
 function openSideMenu() {
   document.getElementById("side-menu").style.left = "0";
-  document.getElementById("reopen-menu").style.display = "none";
 }
 
 // Function to close the side menu
 function closeSideMenu() {
   document.getElementById("side-menu").style.left = "-300px";
-  document.getElementById("reopen-menu").style.display = "block";
 }
-
-// Event listener for the reopen box
-document.getElementById("reopen-menu").addEventListener("click", openSideMenu);
-
-// Event listener for the close button
-document
-  .getElementById("close-button")
-  .addEventListener("click", closeSideMenu);
