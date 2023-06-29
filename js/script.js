73e52f0b2a60f517d53929e7cd26d5e7de448d44
Karen Jo <karenfiksdahl@gmail.com>
2023-06-29 14:01:46 +0200

    header adjusted to viewport

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sideMenu = document.querySelector(".side-menu");
   const wrapper = document.getElementById("wrapper");
+  const header = document.getElementById("header");
 
-  // Event listener for scrolling
+  function adjustHeaderPosition() {
+    if (window.scrollY >= wrapper.offsetHeight && window.innerWidth > 600) {
+      header.style.top = "50px";
+      sideMenu.style.display = "block";
+    } else {
+      header.style.top = "0";
+      sideMenu.style.display = "none";
+    }
+  }
+
+  window.addEventListener("scroll", adjustHeaderPosition);
+  window.addEventListener("resize", adjustHeaderPosition);
+
+  adjustHeaderPosition();
+
+  // Event listener for scrolling, side-menu appears when scrolled under wrapper
   window.addEventListener("scroll", function () {
     const wrapperRect = wrapper.getBoundingClientRect();
 
